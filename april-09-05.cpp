378060b1c149faa2c580d5354d0b10e89e31f471
Reazul Islam <reazulislam137@gmail.com>
2018-04-09 01:41:19 +0600

    first commit

---
+++ april-09-05.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << "\n\n Divide two numbers and print : \n";
+    cout << "------------------------------------\n";
+
+    int a=30, b=10, resdiv;
+
+    resdiv = a/b;
+
+    cout << " The quotient of "<< a << " and "<< b << " is "<< resdiv << endl;
+    return 0;
+}
